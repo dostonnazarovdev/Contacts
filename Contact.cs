f94ef4aadabac2db50261eb008f3f70bfd2bdbde
DostonNazarov <dostondev4477@gmail.com>
2023-12-25 12:06:19 +0500

    contact add done

--- Contact.cs
+++ Contact.cs
@@ -1,6 +1,65 @@
-class Contact{
-  
-  public string name;
-  public string surname;
-  public string phone;
+using System;
+
+class Contact
+{
+
+    public string name;
+    public string surname;
+    public string phone;
+
+
+
+    public void Menu()
+    {
+        System.Console.WriteLine("---- Menu ----");
+        System.Console.WriteLine("1.add Contact");
+        System.Console.WriteLine("2.Delete Contact");
+        System.Console.WriteLine("3.Search Contact");
+        System.Console.WriteLine("4.Contact List");
+        System.Console.WriteLine("0.Exit");
+    }
+
+    public int getAction()
+    {
+        System.Console.Write("choose option: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    public Contact addContacts()
+    {
+        System.Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+
+        System.Console.Write("Enter name: ");
+        string surname = Console.ReadLine();
+
+        System.Console.Write("Enter name: ");
+        string phone = Console.ReadLine();
+
+        Contact contact = new Contact();
+        contact.name = name;
+        contact.surname = surname;
+        contact.phone = phone;
+
+
+        return contact;
+    }
+
+    public Contact deleteContacts()
+    {
+
+
+        return null;
+    }
+
+    public Contact searchContacts()
+    {
+        return null;
+    }
+
+    public Contact contactsLists()
+    {
+        return null;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Dastur
 {
-      class Program{
+    class Program
+    {
 
-        static void Main(){
-            Contact[]contacts = new Contact[5];
+        static void Main()
+        {
+            Contact[] contacts = new Contact[5];
+            Contact contact = new Contact();
             bool b = true;
-            
+
+            while (b)
+            {
+                contact.Menu();
+                int n = contact.getAction();
+                switch (n)
+                {
+                    case 1:
+                        contact.addContacts();
+                        break;
+                    case 2:
+                        contact.deleteContacts();
+                        break;
+                    case 3:
+                        contact.searchContacts();
+                        break;
+                    case 4:
+                        contact.contactsLists();
+                        break;
+                    case 0:
+                        b = false;
+                        break;
+                    default:
+                        System.Console.WriteLine("wrong option!");
+                        break;
+                }
+
+            }
+
+
+
         }
-      }
+    }
 }
